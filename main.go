79f658525c2c19aae29b6466beb217271cca1db8
Pravee Singh <ratinsingh23@gmail.com>
2023-10-16 02:06:05 +0530

    air added and a template added to serve when url is hit

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
+var tpl = template.Must(template.ParseFiles(("index.html")))
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello World!</h1>"))
+	tpl.Execute(w, nil)
+	// w.Write([]byte("<h1>Hello World!</h1>"))
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error laoding .env file")
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
